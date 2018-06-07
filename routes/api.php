bc4c7ff4daa96b3ae90b04b17c3dbc66dfe4313f
tanvir <tanvir.alam.shawn@gmail.com>
2018-06-07 22:24:24 +0200

    Update api route

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::post('login', 'Auth\LoginController@login');
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('logout', 'Auth\LoginController@logout');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('offers', 'Api\OfferController@index');
+    Route::get('offers/{offer}', 'Api\OfferController@show');
+    Route::post('offers', 'Api\OfferController@store');
+    Route::put('offers/{offer}', 'Api\OfferController@update');
+    Route::delete('offers/{offer}', 'Api\OfferController@delete');
 });
