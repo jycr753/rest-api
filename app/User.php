5167bbf054b36e17af5e2c4731b5cd598c48707f
tanvir <tanvir.alam.shawn@gmail.com>
2018-06-07 22:23:10 +0200

    Add method for generating api token

--- app/User.php
+++ app/User.php
@@ -26,4 +26,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Generate user based token
+     *
+     * @return string
+     */
+    public function generateToken()
+    {
+        $this->api_token = str_random(60);
+        $this->save();
+
+        return $this->api_token;
+    }
 }
