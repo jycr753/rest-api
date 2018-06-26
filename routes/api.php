abd57280a544a163ba3cdca7b67f4f3e73f1f41b
Jorge Y. Castillo <jycr753@users.noreply.github.com>
2018-06-26 21:19:35 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,5 @@ Route::post('register', 'Auth\RegisterController@register');
 Route::post('logout', 'Auth\LoginController@logout');
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('offers', 'Api\OfferController@index');
-    Route::get('offers/{offer}', 'Api\OfferController@show');
-    Route::post('offers', 'Api\OfferController@store');
-    Route::put('offers/{offer}', 'Api\OfferController@update');
-    Route::delete('offers/{offer}', 'Api\OfferController@delete');
+    Route::source('offers', 'Api\OfferController');
 });
